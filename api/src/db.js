3e96feed5a2b901a9dff4da18abf8735a2d30c6b
SantinoFajardo <santinofajardo02@gmail.com>
2022-08-13 19:05:30 -0300

    base de datos creada

--- api/src/db.js
+++ api/src/db.js
@@ -37,7 +37,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame } = sequelize.models;
+const { Videogame, Genre } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
