b100c73bcf479f8569c8ab49586e21d0eece06fe
SantinoFajardo <santinofajardo02@gmail.com>
2022-08-30 22:43:25 -0300

    react-create-app on Heroku

--- api/src/db.js
+++ api/src/db.js
@@ -8,8 +8,8 @@ const { DB_USER, DB_PASSWORD, DB_HOST, apikey } = process.env;
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
   {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    logging: false, // set to console.log to see the raw SQL queries.
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed.
   }
 );
 const basename = path.basename(__filename);
